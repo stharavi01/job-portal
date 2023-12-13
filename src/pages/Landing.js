22b6a69981aeaf12727a642105e7d1a7a8365171
Rabin Shrestha <sanayashe@gmail.com>
2023-12-13 11:02:51 +0545

    indentation fixed

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,49 +3,41 @@ import main from "../assets/images/main.svg"
 import styled from "styled-components";
 
 const Wrapper = styled.main`
-nav{
+  nav {
     width: var(--fluid-width);
     max-width: var(--max-width);
     margin: 0 auto;
     height: var(--nav-height);
     display: flex;
     align-items: center;
-}
-
-.page{
-min-height: calc(100vh - var(--nav-height));
-display: grid;
-align-items: center;
-margin-top: -3rem;
-}
-
-h1{
+  }
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  h1 {
     font-weight: 700;
-
-span{
-    color: var(--primary-500);
-}
-
-p{
+    span {
+      color: var(--primary-500);
+    }
+  }
+  p {
     color: var(--grey-600);
-}
-
-.main-img{
+  }
+  .main-img {
     display: none;
-}
-
-@media (min-width: 992px) {
+  }
+  @media (min-width: 992px) {
     .page {
-        grid-template-columns: 1fr 1fr;
-        column-gap: 3rem;
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
     }
-
-    .main-img{
-        display: block;
+    .main-img {
+      display: block;
     }
-}
-
-}
+  }
 `;
 
 const Landing = () => {
