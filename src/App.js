285ab344ecdaeb33feebc0b4f5d9c7aa638fb11e
Rabin Shrestha <sanayashe@gmail.com>
2023-12-13 15:00:39 +0545

    pages setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Landing from "./pages/Landing"
+import { Landing, Error, Dashboard, Register } from "./pages";
 
 const App = () => {
   return (
     <div>
       <Landing />
     </div>
-  )
-}
-export default App 
+  );
+};
+export default App; 
