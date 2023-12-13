fc4331bb34754366525bee28e34a205f16d52cf9
Rabin Shrestha <sanayashe@gmail.com>
2023-12-13 15:28:32 +0545

    Error page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,7 @@
 import { Logo } from "../components";
-import main from "../assets/images/main.svg"
+import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
     return (
@@ -15,14 +16,14 @@ const Landing = () => {
                         job <span>tracking</span>
                     </h1>
                     <p>Crucifix narwhal street art asymmetrical, humblebrag tote bag pop-up fixie raclette taxidermy craft beer. Brunch bitters synth, VHS crucifix heirloom meggings bicycle rights.</p>
-                    <button className="btn btn-hero">Login/Register</button>
+                    <Link to='/register' className="btn btn-hero">Login/Register</Link>
                 </div>
                 <img src={main} alt="job hunt" className="img main-img" />
             </div>
         </Wrapper>
     );
-}
+};
 
 
 
-export default Landing
+export default Landing;
