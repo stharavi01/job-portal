627ac02659a1315d881f880fcd3a9b5693e7ca07
Rabin Shrestha <93304537+stharavi01@users.noreply.github.com>
2024-01-04 01:25:56 +0545

    Error.js

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -10,8 +10,7 @@ const Error = () => {
                 <img src={img} alt="not found" />
                 <h3>Ohh! Page Not Found</h3>
                 <p>We can't seem to find the page you're looking for</p>
-                <Link to='/'>Back Home</Link>
-            </div>
+                <Link to='/'>Back Home</
         </Wrapper>
     );
 };
