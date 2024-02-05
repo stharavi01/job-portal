fc00dba621f7823f434ace35734919242cfcd756
Rabin Shrestha <93304537+stharavi01@users.noreply.github.com>
2024-02-04 19:36:44 -0800

    error update

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,17 +1,21 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import img from "../assets/images/not-found.svg";
 import Wrapper from "../assets/wrappers/ErrorPage";
 
 const Error = () => {
-      const [values, setValues] = useState(initialState);
+    // Assuming initialState is defined somewhere in your code
+    const [values, setValues] = useState(initialState);
+
     return (
         <Wrapper className="full-page">
-            <div>
-                <img src={img} alt="not found" />
-               
-                <p>We can't seem to find the page you're looking for</p>
-                <Link to='/'>Back Home</
+            <div className="error-container">
+                <img src={img} alt="not found" className="error-image" />
+                <p className="error-message">We can't seem to find the page you're looking for</p>
+                <Link to="/" className="back-home-link">Back Home</Link>
+            </div>
         </Wrapper>
     );
 };
+
 export default Error;
