661808ae8311e87f0a4d140facfa4edb0d74aa1b
Rabin Shrestha <93304537+stharavi01@users.noreply.github.com>
2024-02-06 22:21:18 +0545

    Update subLogo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,12 @@
 import logo from "../assets/images/logo.svg"
 
+import subLogo from "../assets/images/subLogo.svg"
 const Logo = () => {
     return <img src={logo} alt="jobster logo" className="logo" />
+}
+
+
+const subLogo = () => {
+    return <img src={subLogo} alt="jobster logo" className="subLogo" />
 }
 export default Logo
