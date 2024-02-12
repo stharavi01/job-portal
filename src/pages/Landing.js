f3febf41c35a7aa22ee749b415dd4499128675bd
Rabin Shrestha <93304537+stharavi01@users.noreply.github.com>
2024-02-12 20:54:37 +0545

    landing add

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -18,6 +18,13 @@ const Landing = () => {
                     <p>Crucifix narwhal street art asymmetrical, humblebrag tote bag pop-up fixie raclette taxidermy craft beer. Brunch bitters synth, VHS crucifix heirloom meggings bicycle rights.</p>
                     <Link to='/register' className="btn btn-hero">Login/Register</Link>
                 </div>
+ <div className="info">
+                    <h1>
+                        job <span>tracking</span>
+                    </h1>
+                    <p>Crucifix narwhal street art asymmetrical, humblebrag tote bag pop-up fixie raclette taxidermy craft beer. Brunch bitters synth, VHS crucifix heirloom meggings bicycle rights.</p>
+                    <Link to='/register' className="btn btn-hero">Login/Register</Link>
+                </div>
                 <img src={main} alt="job hunt" className="img main-img" />
             </div>
         </Wrapper>
